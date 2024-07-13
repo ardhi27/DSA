ef2ef381079b112c46f4523289c807d61e9ee2d6
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2024-07-13 11:16:53 +0800

    Improve code

--- minimum number game/main.js
+++ minimum number game/main.js
@@ -1,15 +1,35 @@
-var minimumNumberGame = function(s){
-    s.sort((a, b) => a - b);
+// var minimumNumberGame = function(s){
+//     s.sort((a, b) => a - b);
 
-    for(let i = 0; i < s.length; i += 2){
-        if(i + 1 < s.length){
-            [s[i], s[i + 1]] = [s[i + 1], s[i]];
+//     for(let i = 0; i < s.length; i += 2){
+//         if(i + 1 < s.length){
+//             [s[i], s[i + 1]] = [s[i + 1], s[i]];
+//         }
+//     }
+//     return s
+// }
+
+
+// const arr = minimumNumberGame([7, 5, 2, 3, 2, 6])
+
+// console.log(arr)
+
+
+var minimumNumberGame = function (numbers) {
+    numbers.sort((a, b) => a - b)
+    const result = []
+    while (numbers.length > 0) {
+        if (numbers.length === 0) {
+            break;
+        } else {
+            let aliceNumber = numbers.shift();
+            let bobNumber = numbers.shift();
+            result.push(bobNumber)
+            result.push(aliceNumber)
         }
+
     }
-    return s
+    return result
 }
 
-
-const arr = minimumNumberGame([7, 5, 2, 3, 2, 6])
-
-console.log(arr)
+console.log(minimumNumberGame([10, 5, 6, 4]))
