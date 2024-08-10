a1f4ad9064cc91c2d971ff6d765143d019efc446
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2024-08-10 12:22:21 +0800

    add new case

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-const arr = [[1, 2, 3, 4, 5], [2, 5, 6, 7, 8]]
+const sentence = "Word"
 
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-        if (arr[i][j] % 2 == 0) {
-            console.log(arr[i])
-            break;
-        }
-    }
+const arraySentence = Array.from(sentence)
+
+let map = new Map();
+
+
+console.log(arraySentence)
+
+for(let i = 0; i < arraySentence.length; i++){
+    map[i] = arraySentence[i]
 }
+
+console.log(map)
